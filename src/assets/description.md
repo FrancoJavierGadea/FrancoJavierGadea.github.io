bc203e6f6b1b4e46d3f325936847d0567a2bd414
1812 <francogadea1812@gmail.com>
2023-09-08 02:32:37 -0300

    boceto inicial

---
+++ src/assets/description.md
@@ -0,0 +1,9 @@
+---
+name: "Franco Javier Gadea"
+---
+
+**Desarrollador web** Fullstack con habilidades en el **frontend** y el **backend**.
+
+Mi interés en el desarrollo web se centra en crear soluciones creativas e innovadoras que mejoren la vida de las personas y hagan crecer los negocios. Me mantengo actualizado en las **últimas tecnologías** y tendencias de industria para crear aplicaciones eficientes y escalables...
+
+En mi portfolio encontrarás algunos de los proyectos en los que he trabajado, desde aplicaciones web simples hasta complejos juegos en línea. Si buscas un desarrollador comprometido, ¡contáctame!
---
+++ src/utils/icons.js
@@ -0,0 +1,49 @@
+
+import react from '../assets/icons/reactjs.svg';
+import html from '../assets/icons/html5.svg';
+import css from '../assets/icons/css3.svg';
+import js from '../assets/icons/js.svg';
+import mongo from '../assets/icons/mongo.svg';
+import mysql from '../assets/icons/mysql.svg';
+import next from '../assets/icons/next.svg';
+import bootstrap from '../assets/icons/bootstrap.svg';
+import express from '../assets/icons/expressjs.svg';
+
+export const TECNOLOGIAS = [ 
+    {
+        name: 'HTML',
+        icon: html
+    },
+    {
+        name: 'CSS',
+        icon: css
+    },
+    {
+        name: 'JavaScript',
+        icon: js
+    },
+    {
+        name: 'React',
+        icon: react
+    },
+    {
+        name: 'Next js',
+        icon: next
+    },
+    {
+        name: 'Express',
+        icon: express
+    },
+    {
+        name: 'MongoDB',
+        icon: mongo
+    },
+    {
+        name: 'MySQL',
+        icon: mysql
+    },
+    {
+        name: 'Bootstrap',
+        icon: bootstrap
+    },
+];
