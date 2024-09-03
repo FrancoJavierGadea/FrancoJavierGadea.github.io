dadc47af439a8547c75782bfdabb4dd293ce8684
1812 <francogadea1812@gmail.com>
2024-09-03 14:27:15 -0300

    Mejorando los temas light y dark

---
+++ src/data/Redes.js
@@ -0,0 +1,32 @@
+
+import curriculum from "@Data/Curriculum-24-11-23.pdf";
+
+export const REDES = [
+    {
+        name: "whatsapp",
+        value: "+54 9 381 606-5882",
+        link: "https://wa.me/5493816065882",
+        bi: "bi-whatsapp",
+    },
+    {
+        name: "mail",
+        value: "francogadea1812@gmail.com",
+        link: "mailto:francogadea1812@gmail.com",
+        bi: "bi-envelope",
+    },
+    {
+        name: "curriculum",
+        link: curriculum,
+        bi: "bi-file-earmark-pdf",
+    },
+    {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/franco-javier-alvarez",
+        bi: "bi-linkedin",
+    },
+    {
+        name: "github",
+        link: "https://github.com/FrancoJavierGadea",
+        bi: "bi-github",
+    }
+];
---
+++ src/data/Technologies/technologies.js
@@ -0,0 +1,76 @@
+import AstroWhite from "./Astro/Astro-white.png";
+import AstroBlack from "./Astro/Astro-black.png";
+import Bootstrap from "./Bootstrap/bootstrap.png";
+import HTML from "./HTML/html.png";
+import CSS from "./CSS/css-white.png";
+import JS from "./Javascript/javascript-black.png";
+import React from "./React/React.png";
+import Typescript from "./Typescript/typescript.png";
+import Wordpress from "./Wordpress/wordpress-white.png";
+
+export const TECHNOLOGIES = [
+    {
+        name: "Astro",
+        url: "https://astro.build/",
+        logo: {
+            dark: AstroWhite.src,
+            light: AstroBlack.src
+        }
+    },
+    {
+        name: "Bootstrap",
+        url: "",
+        logo: {
+            dark: Bootstrap.src,
+            light: null
+        }
+    },
+    {
+        name: "HTML",
+        url: "",
+        logo: {
+            dark: HTML.src,
+            light: null
+        }
+    },
+    {
+        name: "CSS",
+        url: "",
+        logo: {
+            dark: CSS.src,
+            light: null
+        }
+    },
+    {
+        name: "JavaScript",
+        url: "",
+        logo: {
+            dark: JS.src,
+            light: null
+        }
+    },
+    {
+        name: "React",
+        url: "",
+        logo: {
+            dark: React.src,
+            light: null
+        }
+    },
+    {
+        name: "TypeScript",
+        url: "",
+        logo: {
+            dark: Typescript.src,
+            light: null
+        }
+    },
+    {
+        name: "Wordpress",
+        url: "",
+        logo: {
+            dark: Wordpress.src,
+            light: null
+        }
+    },
+];
