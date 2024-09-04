61b8fac479895c34b49bd1ab045bd3371412043c
1812 <francogadea1812@gmail.com>
2024-09-04 00:59:54 -0300

    Agregando Google Analytics y etiquetas para el SEO

---
+++ src/data/SEO.js
@@ -0,0 +1,47 @@
+
+export const SEO = {
+
+    "title": "Franco Javier Gadea",
+
+    "description": "Franco Javier Gadea, Desarrollador Web Full Stack con experiencia en React, Node.js, y la creación de aplicaciones web escalables y de alto rendimiento. Especializado en mejorar la experiencia del usuario y optimizar el rendimiento de aplicaciones modernas.",
+    
+    "type": "website",
+
+    "url": "https://francojaviergadea.github.io/",
+
+    "image": {
+        "url": "https://francojaviergadea.github.io/",
+        "width": "1200",
+        "height": "600",
+        "type": "image/png",
+        "alt": "Franco Javier Álvarez, Desarrollador Web Full Stack especializado en React, Node.js, y aplicaciones web escalables."
+    },
+
+    "locale": "es_AR",
+
+    "keywords": [
+        "Desarrollador Web Full Stack",
+        "Franco Javier Gadea",
+        "React",
+        "Node.js",
+        "Aplicaciones web escalables",
+        "Alto rendimiento",
+        "Experiencia del usuario",
+        "Optimización",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "MongoDB",
+        "MySQL",
+        "Express",
+        "PHP",
+        "WordPress",
+        "Desarrollo frontend",
+        "Desarrollo backend",
+        "Ingeniería de software",
+        "Desarrollo de aplicaciones web"
+    ].join(', '),
+
+    "robots": "index"
+};
