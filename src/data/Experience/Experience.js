bfcdd0c372bbed94361f3ed82fb4aa218c1eb0b9
1812 <francogadea1812@gmail.com>
2024-09-04 22:36:13 -0300

    Mejorando los temas dark y light, las fechas de la experiencia y otros detalles minimos

---
+++ src/data/Experience/Experience.js
@@ -0,0 +1,40 @@
+import krayon from "./Krayion/logo.webp";
+import prospect from "./Prospect/logo.jpg";
+
+export const EXPERIENCE = [
+
+    {
+        title: "Prospect.ar",
+
+        dates: {
+            start: "11/01/2023",
+            end: null
+        },
+
+        url: "https://Prospect.ar/",
+
+        logo: prospect.src,
+
+        job: "Desarrollador web",
+
+        description: null
+    },
+    
+    {
+        title: "Krayion Digital Agency",
+
+        dates: {
+            start: "08/22/2022",
+            end: "11/01/2023"
+        },
+
+        url: "https://krayion.com/",
+
+        logo: krayon.src,
+
+        job: "Desarrollador web",
+
+        description: "Trabajé de manera freelance desarrollando múltiples proyectos como Landing Pages, E-commerces, Blogs, etc... usando sobre todo WordPress y Astro + React."  
+    },
+
+];
--- src/utils/Themes/Themes.js
+++ src/utils/Themes/Themes.js
@@ -1,14 +1,22 @@
 import Gargantua from "@assets/Gargantua.mp4";
 import GargantuaPoster from "@assets/Gargantua-poster.jpg";
+import Pattern3 from "@assets/background-3.svg?url";
+import Pattern2 from "@assets/background-2.svg?url";
 
 export const THEMES = {
     LIGHT: {
         name: 'light',
-        bs: 'light'
+        bs: 'light',
+        background: {
+            poster: Pattern3
+        }
     },
     DARK: {
         name: 'dark',
-        bs: 'dark'
+        bs: 'dark',
+        background: {
+            poster: Pattern2
+        }
     },
     BLACKHOLE: {
         name: 'blackhole',
