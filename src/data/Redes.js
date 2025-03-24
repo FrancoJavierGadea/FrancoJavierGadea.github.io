e463e5a4cfa4d07faa76cbd5f690e15b99364a0b
1812 <francogadea1812@gmail.com>
2025-03-24 14:30:14 -0300

    Actualizando el curriculum y la carta de presentacion

--- src/data/Redes.js
+++ src/data/Redes.js
@@ -12,21 +12,26 @@ export const REDES = [
         name: "mail",
         value: "francogadea1812@gmail.com",
         link: "mailto:francogadea1812@gmail.com",
-        bi: "bi-envelope",
-    },
-    {
-        name: "curriculum",
-        link: curriculum,
-        bi: "bi-file-earmark-pdf",
+        bi: "bi-mailbox",
     },
     {
         name: "linkedin",
-        link: "https://www.linkedin.com/in/franco-javier-alvarez",
+        link: "https://www.linkedin.com/in/franco-javier-gadea",
         bi: "bi-linkedin",
     },
     {
         name: "github",
         link: "https://github.com/FrancoJavierGadea",
         bi: "bi-github",
-    }
+    },
+    {
+        name: "curriculum",
+        link: import.meta.env.PUBLIC_CURRICULUM_URL,
+        bi: "bi-file-earmark-pdf",
+    },
+    {
+        name: "carta de presentacion",
+        link: import.meta.env.PUBLIC_CARTA_URL,
+        bi: "bi-envelope",
+    },
 ];
